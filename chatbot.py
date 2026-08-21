31d847fb2ab41789bf1ce0a393a6098beacdb86e
mdrive660-cmd <235514238+mdrive660-cmd@users.noreply.github.com>
2026-08-21 18:28:28 +0530

    Change model from llama-3.3 to groq/compound

--- chatbot.py
+++ chatbot.py
@@ -13,7 +13,7 @@ chat_completion = client.chat.completions.create(
             "content": "Explain the importance of fast language models",
         }
     ],
-    model="llama-3.3-70b-versatile",
+    model="groq/compound",
 )
 
 print(chat_completion.choices[0].message.content)
